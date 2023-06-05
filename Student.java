489b6456219aa006546635bc4881c3f6bf2512a7
Basana <basana2003@hotmail.com>
2023-06-05 16:46:48 -0400

    added Student.java file

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+	public String name;
+	public long StudentID;
+	public String College;
+	public double cgpa;
+	public static void main(String[] args) {
+		System.out.println("Hello and welcome from student class!");
+	}
+}
