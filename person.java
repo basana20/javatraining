04993a9b1e0d5fc2e8a1ee57201ad97f20287f6e
Basana <basana2003@hotmail.com>
2023-06-05 18:43:31 -0400

    added person.java file

---
+++ person.java
@@ -0,0 +1,7 @@
+public class person {
+    public String name;
+    public String address;
+    public static void main(String[] args) {
+        System.out.println("Hello, I am a person!");
+    }
+}
