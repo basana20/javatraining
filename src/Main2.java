eccce4a774df37492395a411a603fccb77854eb7
Basana <zasmeenkhadka@gmail.com>
2023-06-07 12:09:32 -0400

    updated Main2.java

--- src/Main2.java
+++ src/Main2.java
@@ -2,7 +2,7 @@ public class Main2 {
 
     public static void main(String[] args) {
         System.out.println("Hello and welcome2!");
-        int a = 0;
+        int a = 1;
         int b = 1;
         System.out.println(b/a);
     }
